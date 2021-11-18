f9dee771b8b64550832e2369b3ff07fe97d5009b
snickersnasian <78428693+snickersnasian@users.noreply.github.com>
2021-11-18 10:43:41 +0300

    Shorten code

--- sudoku.js
+++ sudoku.js
@@ -2,92 +2,46 @@ function solve(board) {
     let emptySpot = findEmptySpot(board);
     let row = emptySpot[0];
     let col = emptySpot[1];
-
-    // Ели нет пустых ячеек
     if (row === -1){
         return board;
     }
-
     for(let num = 1; num <= 9; num++){
         if (checkValue(board, row, col, num)){
             board[row][col] = num;
             solve(board);
         }
     }
-
     if (findEmptySpot(board)[0] !== -1)
         board[row][col] = 0;
-
     return board;
 }
-
-
 function checkValue(board, row, column, value) {
-    if(checkRow(board, row, value) &&
-      checkColumn(board, column, value) &&
-      checkSquare(board, row, column, value)) {
-        return true;
-    }
-    
-    return false; 
-};
-
-function checkSquare(board, row, column, value){
     boxRow = Math.floor(row / 3) * 3;
     boxCol = Math.floor(column / 3) * 3;
-    
     for (var r = 0; r < 3; r++){
         for (var c = 0; c < 3; c++){
             if (board[boxRow + r][boxCol + c] === value)
                 return false;
         }
     }
-
-    return true;
-};
-
-function checkColumn(board, column, value){
     for(var i = 0; i < board.length; i++) {
         if(board[i][column] === value) {
             return false;
         }
     }
-
-    return true;
-};
-
-function checkRow(board, row, value){
     for(var i = 0; i < board[row].length; i++) {
         if(board[row][i] === value) {
             return false;
         }
     }
-   
-    return true;
-}
-
+    return true; 
+};
 function findEmptySpot(board) {
-
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 9; j++) {
             if (board[i][j] === 0) 
                 return [i, j];
         }
     }
-
     return [-1, -1];
 }
-
-let res = solve([
-    [1, 7, 9, 0, 0, 0, 0, 4, 3],
-    [0, 6, 0, 0, 0, 0, 0, 2, 1],
-    [4, 0, 2, 0, 0, 1, 9, 8, 0],
-    [6, 0, 7, 0, 0, 0, 0, 1, 0],
-    [5, 0, 0, 0, 1, 2, 3, 9, 0],
-    [3, 0, 1, 4, 9, 7, 0, 0, 0],
-    [9, 0, 0, 3, 0, 5, 0, 7, 4],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 5, 8],
-], 0)
-
-console.log(res);
